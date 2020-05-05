709ccfc52acf6cea1954ea5bf83122569520c075
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2020-05-05 18:11:29 -0500

    Configuración de typescript y express

---
+++ src/server/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+var app: express.Application = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("App is listening on port 3000!");
+});
